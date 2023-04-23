3303b7766bf53b3ed30515fbc81817af6b0d811a
meerkat.log <77188221+meerkat-log@users.noreply.github.com>
2023-04-23 21:44:00 +0900

    Create 피로도.py

---
+++ Programmers/완전탐색/피로도.py
@@ -0,0 +1,27 @@
+answer = 0
+
+
+def dfs_dungeons(k, dungeons, count, clear):
+    global answer
+
+    for i in range(len(dungeons)):
+        if answer < count:
+            answer = count
+
+        if clear[i] == 0 and k >= dungeons[i][0]:
+            clear[i] = 1
+            dfs_dungeons(k - dungeons[i][1], dungeons, count + 1, clear)
+            clear[i] = 0
+
+
+def solution(k, dungeons):
+    global answer
+
+    for i in range(len(dungeons)):
+        clear = [0] * len(dungeons)
+        clear[i] = 1
+        count = 1
+
+        dfs_dungeons(k - dungeons[i][1], dungeons, count, clear)
+
+    return answer
