c1f5213d2a78acb7763ce45abff3ae927a456927
meerkat.log <77188221+meerkat-log@users.noreply.github.com>
2023-04-23 21:45:25 +0900

    Create 체육복.py

---
+++ Programmers/탐욕법(Greedy)/체육복.py
@@ -0,0 +1,14 @@
+def solution(n, lost, reserve):
+    answer = 0
+    lost.sort()
+    reserve.sort()
+    
+    _lost = [i for i in lost if i not in reserve]
+    _reserve = [i for i in reserve if i not in lost]
+
+    for i in _reserve:
+        if i - 1 in _lost:
+            _lost.remove(i-1)
+        elif i + 1 in _lost:
+            _lost.remove(i+1)
+    return n - len(_lost)
